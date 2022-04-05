17ec1396b72911481110f560a4e38603077fee5a
Mariele <mariele.fonttana@gmail.com>
2022-04-05 22:54:01 +0100

    answer questions

--- src/App.js
+++ src/App.js
@@ -9,12 +9,18 @@ export default function Interview() {
     <>
       <h2>Welcome to your first simple interview</h2>
       <h3>Tell me about yourself</h3>
-      <p>...</p>
+      <p>
+        My background is software developer. I have been working with react to
+        improve my front-end developer skills
+      </p>
       <h3>What language is your favorite ? </h3>
-      <p>Currently i'm coding using react because AS EMPRESAS UTILIZAM</p>
+      <p>
+        Currently i'm coding using react because is a lib very adptable and
+        powerfull. In addition the comunity have a large support.
+      </p>
       <h3>List your soft skills</h3>
 
-      <p>...............</p>
+      <p>I am TeamWork, adaptability, work ethic</p>
       <Img src="https://cdn-blog.novoresume.com/articles/soft-skills/soft-skills.png" />
     </>
   );
